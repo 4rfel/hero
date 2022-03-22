e271109bedf26b59d7fc7ee214d0012647641a71
4rfel <rafael_1999_@hotmail.com.br>
2022-03-22 17:13:35 -0300

    initial

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Draws a 5 unit long red line in front of the object
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, transform.forward * 10);
+    }
+}
